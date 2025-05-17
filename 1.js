912c0c61df26437b4dd62163d182434bcb85f579
PatrickRosa1 <patrickrosa637@gmail.com>
2025-05-17 20:37:52 -0300

    first commit

---
+++ 1.js
@@ -0,0 +1,22 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Digite um número inteiro: ", function(input) {
+  const numero = parseInt(input); 
+  
+  if (isNaN(numero)) {
+    console.log("Por favor, digite um número válido.");
+  } else {
+    if (numero % 2 === 0) {
+      console.log(`${numero} é par.`);
+    } else {
+      console.log(`${numero} é ímpar.`);
+    }
+  }
+
+});
